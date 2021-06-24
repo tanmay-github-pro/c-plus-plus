472fbbabd54ab1399e401d4b75aa1ccc53d52740
Tanmay <tanmayvashistha.mail@gmail.com>
2021-06-24 22:08:32 +0530

    CodeChef - SquareRoot of number | Code - FSQRT | Level - Beginner

---
+++ CodeChef/SquareRootOfNum-FSQRT/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    short n;
+    cin >> n;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        cout << round(sqrt(x)) << "\n";
+    }
+    return 0;
+}
