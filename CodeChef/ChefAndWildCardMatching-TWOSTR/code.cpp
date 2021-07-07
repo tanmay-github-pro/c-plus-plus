dface326875778f53c967b1d7b5a383276b57d7c
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-07 19:22:49 +0530

    CodeChef Problem - Chef and Wild Card Matching | Code - TWOSTR | Level - Beginner

---
+++ CodeChef/ChefAndWildCardMatching-TWOSTR/code.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string str1, str2;
+    cin >> str1 >> str2;
+    bool flag = true;
+    for (int i = 0; i < str1.length(); i++)
+    {
+        if (str1[i] != str2[i] && str1[i] != '?' && str2[i] != '?')
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+
+    return 0;
+}
