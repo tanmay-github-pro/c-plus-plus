b62fe58c6299dcff1e11253edc3107a9c7161497
Tanmay <tanmayvashistha.mail@gmail.com>
2021-09-10 17:35:44 +0530

    Quick sort performed on an array

---
+++ Sorting Algorithms/Quick Sort/quickSort.cpp
@@ -0,0 +1,91 @@
+// Sorting elements in an array using quick sort
+#include <iostream>
+using namespace std;
+
+int partition(int a[], int l, int r);
+void quickSort(int a[], int l, int r);
+void display(int a[], int n);
+void swap(int *a, int *b);
+
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    int a[n];
+    cout << "\nEnter elements in array\n";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    quickSort(a, 0, n - 1);
+    display(a, n);
+    return 0;
+}
+
+/*  Function will return updated position of pivot so that partitions can be made 
+    and quick sort will be performed on them
+ */
+/*
+int partition(int a[], int l, int r)
+{
+    // last element of array is taken to be pivot
+    int pivot = a[r];
+    // i will point to last number which is smaller than pivot
+    int i = l - 1;
+    for (int j = l; j < r; j++)
+    {
+        if (a[j] < pivot)
+        {
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[r]);
+    return i + 1;
+}
+*/
+
+// this also works (self written code)
+int partition(int a[], int l, int r)
+{
+    int pivot = a[r];
+    int i = l-1, j = r;
+    do
+    {
+        do
+        {
+            i++;
+        } while (a[i] < pivot);
+        do
+        {
+            j--;
+        } while (a[j] > pivot && j != -1);
+        if(i < j)
+            swap(a[i], a[j]);
+    } while (i < j);
+    swap(a[j+1], a[r]);
+    return j+1;    
+}
+
+void quickSort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int pi = partition(a, l, r);
+        quickSort(a, l, pi - 1);
+        quickSort(a, pi + 1, r);
+    }
+}
+
+void display(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+       cout << a[i] << "\t";
+    cout << "\n";    
+}
+
+void swap (int *a, int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
